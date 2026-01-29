242538ccc655ac71adf7f28b1118224583c11e92
rdeiser <rdeiser@ksu.edu>
2026-01-29 12:37:42 -0600

    feat(pay-fines-component,-customComponentMappings.ts): this component adds a pay fines hyperlink within the patron's fines and fees module

--- src/app/custom1-module/customComponentMappings.ts
+++ src/app/custom1-module/customComponentMappings.ts
@@ -1,6 +1,8 @@
 import { Libraryh3lpComponent } from '../libraryh3lp/libraryh3lp.component';
+import { PayFinesComponent } from '../pay-fines/pay-fines.component';
 
 // Define the map
 export const selectorComponentMap = new Map<string, any>([
   ['nde-footer-after', Libraryh3lpComponent],
+  ['nde-account-section-results-before', PayFinesComponent],
 ]);
---
+++ src/app/pay-fines/pay-fines.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'custom-pay-fines',
+  standalone: true,
+  imports: [],
+  templateUrl: './pay-fines.component.html',
+  styleUrl: './pay-fines.component.scss'
+})
+export class PayFinesComponent {
+
+}
